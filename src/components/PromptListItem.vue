cee85311d99acdb63bdb2438ea1c93070eff0ba1
Denis Sobolevsky <desobolevsky@gmail.com>
2023-04-12 16:57:33 +0300

    add prompts list and prompts list item

---
+++ src/components/PromptListItem.vue
@@ -0,0 +1,9 @@
+<template>
+    <li :id="id">{{ title }}</li>
+</template>
+
+<script>
+export default {
+    props: ['title', 'id']
+}
+</script>
---
+++ src/components/PromptsList.vue
@@ -0,0 +1,19 @@
+<template>
+    <PromptListItem
+      v-for="prompt in prompts"
+      :key="prompt.id"
+      :title="prompt.title"
+      :id="prompt.id"
+       />
+</template>
+
+<script>
+import PromptListItem from './PromptListItem.vue';
+
+export default {
+    components: {
+        PromptListItem
+    },
+    props: ['prompts']
+}
+</script>
