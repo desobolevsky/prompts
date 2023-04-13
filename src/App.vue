9e2fd83786428898012e4b8199007e61c0637b4a
Denis Sobolevsky <desobolevsky@gmail.com>
2023-04-13 15:44:35 +0300

    add prompt description

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,41 @@
 <template>
   <a>Selected Prompt Id: {{ selectedPromptId }}</a>
+  <PromptDescripton v-if="prompt" :prompt="prompt"/>
   <PromptsList :selectedPromptId="selectedPromptId" @selectPrompt="selectPrompt"/>
 </template>
 
 <script>
-import PromptsList from './components/PromptsList.vue'
+import PromptsList from './components/PromptsList.vue';
+import PromptDescripton from './components/PromptDescripton.vue';
+import { PROMPTS_API_LIST_PROMPTS_URL } from './settings';
+
+const axios = require('axios');
 
 export default {
   components: {
-    PromptsList
-  },
+    PromptsList,
+    PromptDescripton
+},
   data() {
     return {
       selectedPromptId: null,
+      prompt: null,
     }
   },
   methods: {
     selectPrompt(promptId){
       this.selectedPromptId = promptId;
+      this.getPrompt(promptId);
+    },
+    getPrompt(promptId){
+      const getPromptUrl = `${PROMPTS_API_LIST_PROMPTS_URL}${promptId}`;
+      axios.get(getPromptUrl)
+          .then(response => {
+              (this.prompt = response.data);
+          })
+          .catch(error => {
+              console.error(error);
+          });
     }
   }
 }
---
+++ src/components/PromptDescripton.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h2>{{ prompt.title }}</h2>
+      <p>{{ prompt.description }}</p>
+    </div>
+  </template>
+
+<script>
+export default {
+    props: ['prompt']
+}
+</script>
