daba07045dec0b28adeafca94b4dd1139d747b9c
Dzianis <desobolevsky@gmail.com>
2023-04-27 17:44:56 +0300

    add styles to elements

--- src/components/PromptPlayground.vue
+++ src/components/PromptPlayground.vue
@@ -1,10 +1,36 @@
-<template>  
-  <div v-if="prompt">
-    <h1>{{ prompt.title }}</h1>
-    <h3>{{ prompt.template }}</h3>
-    <div v-for="(example, index) in prompt.examples" :key="index">
-      <p>User: {{ example.user }}</p>
-      <p>ChatGPT: {{ example.system }}</p>
+<template>
+  <div v-if="prompt" class="playground-box">
+    <div class="title-box">
+      <h1 class="title">{{ prompt.title }}</h1>
+    </div>
+    <div class="template-box">
+      <div class="template-wrapper">
+        <span class="template-hint">template</span>
+        <h2 class="template-text">{{ prompt.template }}</h2>
+      </div>
+    </div>
+
+    <div class="examples-box">
+      <div v-for="(example, index) in prompt.examples" :key="index" class="example-box">
+        <div class="example-content">
+          <div class="message-box-user">
+            <div class="message-part">
+              <img :src="require('@/assets/user-icon.png')" alt="User avatar">
+            </div>
+            <div class="message-part message-text">
+              <p>{{ example.user }}</p>
+            </div>
+          </div>
+          <div class="message-box-chatgpt">
+            <div class="message-part">
+              <img :src="require('@/assets/chatgpt-icon.svg')" alt="ChatGPT avatar">
+            </div>
+            <div class="message-part message-text">
+              <p>{{ example.system }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
   <p v-else>Loading</p>
@@ -12,6 +38,6 @@
 
 <script>
 export default {
-    props: ['prompt']
+  props: ['prompt']
 }
 </script>
