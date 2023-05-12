a8d8cc35ab3d496b18672a0251b842f31cf87aea
Dzianis Sabaleuski <desobolevsky@gmail.com>
2023-05-12 20:00:32 +0300

    add backend Dockerfile

---
+++ backend/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Check if the migrations have already been applied
+if [ ! -f alembic/versions/$(poetry run alembic current | awk '{print $4}') ]; then
+    echo "Upgrading database with Alembic"
+    poetry run alembic upgrade head
+fi
+
+poetry run uvicorn main:app --host 0.0.0.0 --port 8000
